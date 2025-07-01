4dee79df20a91607e77cdaac9dd44a7cf84ca844
Lucas Ariel García <lg140799@gmail.com>
2025-06-30 23:47:11 -0300

    Update ejs.java

--- src/uso/ejs.java
+++ src/uso/ejs.java
@@ -209,4 +209,16 @@ public class ejs {
         }
         return grados;
     }
+    
+    public static int ejercicio13(ABBTDA arbol) {
+        if (arbol.arbolVacio()) {
+            return 0;
+        }
+        if (arbol.hijoIzq().arbolVacio() && arbol.hijoDer().arbolVacio()) {
+            if (arbol.raiz() % 2 == 0) return 1;
+            else return 0;
+        }
+        return contarHojasPares(arbol.hijoIzq()) + contarHojasPares(arbol.hijoDer());
+    }
+    
 }
